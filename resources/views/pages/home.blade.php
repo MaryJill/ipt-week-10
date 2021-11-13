2bfb7e534c097298303fe22f29d0019625660ad2
unknown <chadiegilaugis@gmail.com>
2021-11-13 11:43:03 +0800

    bladed

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+    <div class="d-flex align-items-center justify-content-center">
+        <img src="{{ asset('images/header.png') }}" alt="">
+    </div>
+
+@endsection
